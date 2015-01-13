b58e8261bda418028838ae6ebc74926e55be4084
mrGhosd <vforvad@gmail.com>
2015-01-13 20:36:10 +0300

    refactor recipes controller specs and add more specs fpr invalid attributes

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -30,14 +30,28 @@ describe RecipesController do
   describe "PUT #update" do
     context "with valid attributes" do
       it "update attributes of recipe" do
-        put :update, id: recipe.id, recipe: attributes_for(:recipe, title: "ololo")
+        put :update, id: recipe.id, title: "ololo", description: "awdawdawd"
         recipe.reload
         expect(recipe.title).to eq("ololo")
       end
 
       it "return 200 status" do
-        put :update, id: recipe.id, recipe: attributes_for(:recipe)
+        put :update, id: recipe.id, title: "ololo", description: "awdawdawd"
         expect(response.status).to eq(200)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't update attributes of recipe" do
+        put :update, id: recipe.id, title: "", description: "awdawdawd"
+        recipe.reload
+        expect(recipe.title).to eq(recipe.title)
+      end
+
+      it "return 200 status" do
+        put :update, id: recipe.id, title: "", description: "awdawdawd"
+        recipe.reload
+        expect(JSON.parse(response.body)).to have_key("title")
       end
     end
   end
@@ -45,13 +59,23 @@ describe RecipesController do
   describe "POST #create" do
     context "with valid attributes" do
       it "create a new recipe" do
-        expect{post :create, recipe: attributes_for(:recipe, title: recipe.title,
-        description: 'description')}.to change(Recipe, :count).by(1)
+        expect{post :create, {title: "ololo", description: "awdawdawd"}}.to change(Recipe, :count).by(1)
       end
 
       it "return status 200" do
-        post :create, recipe: attributes_for(:recipe)
+        post :create, {title: "ololo", description: "awdawdawd"}
         expect(response.status).to eq(200)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't create a new recipe" do
+        expect{post :create, {title: "", description: "awdawdawd"}}.to change(Recipe, :count).by(0)
+      end
+
+      it "return new recipe errors" do
+        post :create, title: "", description: "awdawdawd"
+        expect(JSON.parse(response.body)).to have_key("title")
       end
     end
   end
