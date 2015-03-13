82a8a5af13d827588a42f1b35b0c8cb4847656e4
mrGhosd <vforvad@gmail.com>
2015-03-13 07:15:56 +0300

    add 'acts_as_taggable' gem and introduce tag model

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem "markitup-rails"
 gem "devise"
+gem 'acts-as-taggable-on'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,9 @@ class Recipe < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :steps, dependent: :destroy
   has_one :image, as: :imageable, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings
+  acts_as_taggable
 
   validates :title, :description, presence: true
 
@@ -11,5 +14,15 @@ class Recipe < ActiveRecord::Base
   def images
     {small: self.image.url(:small),
     normal: self.image.url(:normal)}
+  end
+
+  def tag_list
+    self.tags.map(&:name).join(", ")
+  end
+
+  def tag_list=(value)
+    self.tags = names.split(",").map do |n|
+      ActsAsTaggableOn::Tag.where(name: n.strip).first_or_create!
+    end
   end
 end
