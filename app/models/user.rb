7944c5afd580647eb1fedb58aead728f544df099
mrGhosd <vforvad@gmail.com>
2015-03-26 18:20:38 +0300

    add relationship entity and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,28 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes
   has_many :authorizations
+  has_many :relationships, foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :following, through: :relationships, source: :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+           :class_name => "Relationship",
+           :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   include Rate
+
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
 
 
   def correct_naming
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321175857) do
+ActiveRecord::Schema.define(version: 20150326151041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,17 @@ ActiveRecord::Schema.define(version: 20150321175857) do
 
   add_index "recipes", ["category_id"], name: "index_recipes_on_category_id", using: :btree
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id", using: :btree
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "steps", force: true do |t|
     t.integer  "recipe_id"
