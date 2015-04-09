99a298b4d22ca1decc41a8adaab7fda0c5068705
mrGhosd <vforvad@gmail.com>
2015-04-10 01:29:41 +0300

    add localization for left navigation menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session, if: lambda{ |controller| controller.controller_name != "images" }
+  before_action :set_locale
 
+  def set_locale
+    I18n.locale = cookies[:locale] || session[:locale] || I18n.default_locale
+  end
 end
