9caab1e723b8a10d5a7769a114a266729735ea2e
mrGhosd <vforvad@gmail.com>
2015-05-03 02:14:27 +0300

    add empty specs file for news and callbacks

---
+++ spec/factories/callback.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :callback do
+    author "Author"
+    text  "Text"
+  end
+end
---
+++ spec/factories/news.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :news do
+    title "Title"
+    text  "Text"
+    rate 0
+  end
+end
