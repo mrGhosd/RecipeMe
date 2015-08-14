496720dc1463e58b9d87e2e2249172ff8f75e55c
vsokoltsov <vforvad@gmail.com>
2015-08-14 14:57:15 +0300

    remove useless code from ingridients controller and specs

--- app/controllers/ingridients_controller.rb
+++ app/controllers/ingridients_controller.rb
@@ -1,48 +1,7 @@
 class IngridientsController < ApplicationController
-  before_action :load_recipe, only: [:create, :recipe_ingridients, :destroy]
-  before_action :load_ingridient, only: [:update, :destroy]
+  before_action :load_recipe, only: [:destroy]
+  before_action :load_ingridient, only: [:destroy]
 
-  def index
-    @ingridients = Ingridient.all
-    render json: @ingridients.as_json, status: :ok
-  end
-
-  def recipe_ingridients
-    render json: @recipe.ingridients_list.as_json, status: :ok
-  end
-
-  def create
-    common_ingridient = Ingridient.find_by name: params[:name]
-    @ingridient = @recipe.ingridients.find_by name: params[:name]
-    if @ingridient.present? && common_ingridient.present?
-      render json: @ingridient.as_json, status: :ok
-    elsif @ingridient.blank? && common_ingridient.present?
-      add_ingridient_to_recipe(common_ingridient)
-      @ingridient = common_ingridient
-      render json: @ingridient.as_json, status: :ok
-    else
-      @ingridient = Ingridient.new(ingridient_params)
-      if @ingridient.save
-        add_ingridient_to_recipe(@ingridient)
-        render json: @ingridient.as_json, status: :ok
-      else
-        render json: @ingridient.errors.as_json, status: :unprocessable_entity
-      end
-    end
-  end
-
-  def show
-    ingridient = Ingridient.find(params[:id])
-    render json: ingridient.as_json, status: :ok
-  end
-
-  def update
-    if @ingridient.update(ingridient_params)
-      render json: @ingridient.as_json, status: :ok
-    else
-      render json: @ingridient.errors.as_json, status: :unprocessable_entity
-    end
-  end
 
   def destroy
     Recipe.find(params[:recipe_id]).ingridients.find(params[:id]).destroy
@@ -52,11 +11,6 @@ class IngridientsController < ApplicationController
 
   private
 
-  def add_ingridient_to_recipe(ingridient)
-    recipe = Recipe.find(params[:recipe_id])
-    recipe.recipe_ingridients.create(ingridient: ingridient, size: params[:in_size])
-  end
-
   def load_ingridient
     @ingridient = Ingridient.find(params[:id])
   end
@@ -64,9 +18,4 @@ class IngridientsController < ApplicationController
   def load_recipe
     @recipe = Recipe.find(params[:recipe_id])
   end
-
-  def ingridient_params
-    params.permit(:name)
-  end
-
 end
