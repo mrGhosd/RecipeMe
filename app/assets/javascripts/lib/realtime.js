1a849dedca3f8b40353195a846f136e5691684ad
vsokoltsov <vforvad@gmail.com>
2015-09-07 00:32:32 +0300

    change realtime client

--- app/assets/javascripts/lib/realtime.js
+++ app/assets/javascripts/lib/realtime.js
@@ -1,6 +1,9 @@
 window.RecipeMe.realtime = {
     connect : function(){
-        window.RecipeMe.socket = io.connect("http://188.166.99.8:5001");
+        window.RecipeMe.socket = io.connect("http://188.166.99.8", {
+            'port': '5001',
+            'transports': ['websockets']
+        });
         //window.RecipeMe.socket = io.connect("http://127.0.0.1:5001");
 
         window.RecipeMe.socket.on("rtchange", function(message){
