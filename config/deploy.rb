eae698968d8042742b391062f86f5e56c39d55e1
vsokoltsov <vforvad@gmail.com>
2015-09-14 00:38:59 +0300

    fix some tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 # config valid only for current version of Capistrano
+require 'json'
 lock '3.4.0'
 
 set :application, 'RecipeMe'
@@ -54,5 +55,53 @@ namespace :deploy do
   # after :restart, :run_thinking_sphinx
   # after :run_thinking_sphinx, :run_ts_deltas
   after :publishing, :run_nodejs_server
+
+end
+
+namespace :pm2 do
+
+  def app_status
+    within current_path do
+      ps = JSON.parse(capture :pm2, :jlist, fetch(:app_command))
+      if ps.empty?
+        return nil
+      else
+        # status: online, errored, stopped
+        return ps[0]["pm2_env"]["status"]
+      end
+    end
+  end
+
+  def restart_app
+    within current_path do
+      execute :pm2, :restart, fetch(:app_command)
+    end
+  end
+
+  def start_app
+    within current_path do
+      execute :pm2, :stop, fetch(:app_command)
+    end
+  end
+
+  desc 'Restart app gracefully'
+  task :restart do
+    on roles(:app) do
+      case app_status
+        when nil
+          info 'App is not registerd'
+          start_app
+        when 'stopped'
+          info 'App is stopped'
+          restart_app
+        when 'errored'
+          info 'App has errored'
+          restart_app
+        when 'online'
+          info 'App is online'
+          restart_app
+      end
+    end
+  end
 
 end
