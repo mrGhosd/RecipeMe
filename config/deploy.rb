2015f2bbfae19c568624a7ac4c0f80593c3b5333
vsokoltsov <vforvad@gmail.com>
2015-09-13 22:22:09 +0300

    fix config for deplot

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
   desc "Run thinking sphinx"
   task :run_thinking_sphinx do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "cd ~/home/deploy/recipeme/current && rake ts:rebuild RAILS_ENV=production && bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production -q ts_delta"
+      execute "cd /home/deploy/recipeme/current && rake ts:rebuild RAILS_ENV=production && bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production -q ts_delta"
     end
   end
 
