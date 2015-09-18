a3b306995bbc52b4d676dea42817bbded4215738
vsokoltsov <vforvad@gmail.com>
2015-09-18 16:32:49 +0300

    fix slog translate module

--- app/models/concerns/slug_translate.rb
+++ app/models/concerns/slug_translate.rb
@@ -1,4 +1,12 @@
 module SlugTranslate
+  extend ActiveSupport::Concern
+
+  before_save :change_slug
+
+  def change_slug
+    self.slug = normalize_friendly_id(self.title)
+  end
+
   def normalize_friendly_id(text)
     text.to_slug.normalize(transliterations: :russian).to_s
   end
