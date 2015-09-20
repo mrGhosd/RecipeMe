d7fd68e5cba97f7381072468122a1d3b325d2459
vsokoltsov <vforvad@gmail.com>
2015-09-20 13:29:10 +0300

    fix issuw with slug translate

--- app/models/concerns/slug_translate.rb
+++ app/models/concerns/slug_translate.rb
@@ -1,15 +1,15 @@
 module SlugTranslate
   extend ActiveSupport::Concern
-  
+
   included do
     before_save :change_slug
   end
 
   def change_slug
-    self.slug = normalize_friendly_id(self.title)
+    self.slug = normalize_friendly_id(self.try(:title) || self.try(:nickname))
   end
 
   def normalize_friendly_id(text)
-    text.to_slug.normalize(transliterations: :russian).to_s
+    text.to_slug.normalize(transliterations: :russian).to_s if text.present?
   end
 end
